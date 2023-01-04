6876c0c18536743d37e51e002a648848fedc0c7d
Sairam Tagline <sairam.tagline@gmail.com>
2023-01-04 18:27:25 +0530

    host

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: .</div>
-    <div>Framework: react</div>
+    <div>Name: mc-host</div>
+    <div>Framework: react-host</div>
     <div>Language: JavaScript</div>
     <div>CSS: Tailwind</div>
   </div>
